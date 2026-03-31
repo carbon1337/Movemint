d7b456609f9b3ee780a95f7e5da71ab873dd59b2
Riley Clarke <24clarkr@gmail.com>
2026-03-30 23:26:27 -0500

    Added new win objective with animation and sound. Other bug fixes.

---
+++ Assets/_Scripts/Managers/WinDetector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinDetector : MonoBehaviour
+{
+    private GameManager GM;
+
+    void Awake() {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Win")) {
+            Debug.Log("you won!");
+            GM.Win();
+            col.GetComponent<AudioSource>().Play();
+            Destroy(col.gameObject, 0.1f);
+        }
+    }
+}
