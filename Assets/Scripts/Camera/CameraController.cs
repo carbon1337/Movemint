b96784560ba2e3025c00d282f8695afa7cb47d93
Riley Clarke <24clarkr@gmail.com>
2026-03-30 22:11:12 -0500

    Added level select, level 2, additional camera settings.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Vector3 offset = new Vector3(0f, 5f, -10f);
-    public float smoothTime = 0.3f;
+    public float xSmoothTime = 0.1f;
+    public float ySmoothTime = 0.3f;
     private Vector3 velocity = Vector3.zero;
 
     public Transform target;
@@ -16,7 +17,7 @@ public class CameraController : MonoBehaviour
 
     void Awake()
     {
-        //Initialize camera start pos for clamp
+        // Initialize camera start pos for clamp
         cameraStartPosY = transform.position.y;
     }
 
@@ -24,21 +25,31 @@ public class CameraController : MonoBehaviour
     void Update()
     {
 
-        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3();
+        Vector3 cameraPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
         if(cameraFollowsY)
         {
-            //Initialize camerYClamped
+            // Initialize camerYClamped
             float cameraYClamped = 0f;
 
-            //Calulate the new clamped cameraY value
-            cameraYClamped = Mathf.Clamp(cameraYClamped,  targetPosition.y, cameraStartPosY);
+            // Calulate the new clamped cameraY value
+            cameraYClamped = Mathf.Clamp(cameraYClamped,  target.position.y, cameraStartPosY);
 
-            //Update target position with clamped cameraY
+            // Update target position with clamped cameraY
             targetPosition = new Vector3(target.position.x, cameraYClamped, transform.position.z);
         }
+        else
+        {
+            // Locks the y axis if camera follows y is off
+            targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        }
         
-        //Move camera accordingly
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        // Seperate damping for y and x values
+        cameraPosition.x = Mathf.SmoothDamp(transform.position.x, targetPosition.x, ref velocity.x, xSmoothTime);
+        cameraPosition.y = Mathf.SmoothDamp(transform.position.y, targetPosition.y, ref velocity.y, ySmoothTime);
+
+        transform.position = cameraPosition;
+
     }
 }
--- Assets/Scripts/Managers/MenuController.cs
+++ Assets/Scripts/Managers/MenuController.cs
@@ -25,16 +25,13 @@ public class MenuController : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void GoToLevelSelect() {
-        SceneManager.LoadScene("LevelSelect");
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene("Level " + level.ToString());
     }
 
     public void GoToNextLevel() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void ToggleOptionsMenu() {
-        //if the menu is off, turn on, if the menu is on, turn off.
     }
 
     public void ToggleLeaderboardMenu() {
@@ -42,10 +39,6 @@ public class MenuController : MonoBehaviour
     }
     public void ClearData() {
         PlayerPrefs.DeleteAll();
-    }
-
-    public void SafetyCheck() {
-        // activate a panel to make sure the player wants to do said action.
     }
 
     public void ExitGame() {
